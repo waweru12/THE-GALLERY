5bee17ae9de82a8293a089125324955282e2615b
Santa Claus <santa@northpole.com>
2019-06-14 11:28:20 +0300

    started project

---
+++ appgallery/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppgalleryConfig(AppConfig):
+    name = 'appgallery'
---
+++ appgallery/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Image(models.Model):
+    ima_name = models.CharField(max_length=33)
+    ima_description = models.TextField()
+    ima_location = models.ForeignKey(location)
+    ima_category = models.ForeignKey(category)
+
+
+    def__str__ (self):
+      return self.ima_name
+               
+
