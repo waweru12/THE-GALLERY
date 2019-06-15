dd96bd7b10ff63464c91329191600b8cfe0c9454
Santa Claus <santa@northpole.com>
2019-06-15 21:03:49 +0300

    added location class

--- appgallery/models.py
+++ appgallery/models.py
@@ -11,5 +11,23 @@ class Image(models.Model):
 
     def __str__ (self):
       return self.ima_name
-               
+
+    def save_location(self):
+      self.save()
+
+
+
+class Location(models.Model):
+    location_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.location_name
+    
+    def save_location(self):
+        self.save()
+
+
+
+        
+
 
