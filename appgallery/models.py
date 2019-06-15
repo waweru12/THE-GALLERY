c6068828c6273df4097076115969ca9ad3d9e2ae
Santa Claus <santa@northpole.com>
2019-06-15 21:04:12 +0300

    class for category

--- appgallery/models.py
+++ appgallery/models.py
@@ -28,6 +28,16 @@ class Location(models.Model):
 
 
 
+class Category(models.Model):
+
+    category_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.category_name
+    
+    def save_category(self):
+        self.save()
+               
         
 
 
