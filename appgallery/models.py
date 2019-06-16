1689796d0f9cfbf16f0fb25126a0c4c6accb2a53
Santa Claus <santa@northpole.com>
2019-06-16 20:17:00 +0300

    added classes to get location and image

--- appgallery/models.py
+++ appgallery/models.py
@@ -20,6 +20,49 @@ class Image(models.Model):
 
 
 
+    @classmethod
+    def get_image_by_id(cls,id):
+        image_result = cls.objects.get(id=id)
+        return image_result
+    @classmethod
+    def get_all_images(cls):
+        all_images = Image.objects.all()
+        for image in all_images:
+            return image
+    
+    @classmethod
+    def search_by_category(cls,search_term):
+        images = cls.objects.filter(category__icontains=search_term)
+        return images
+
+    @classmethod
+    def get_with_location(cls,location):
+        images = cls.objects.filter(location__location_name__icontains=location)
+        return images
+
+    @classmethod
+    def get_with_category(cls,category):
+        images = cls.objects.filter(category__category_name__icontains=category)
+        return images
+    @classmethod
+    def get_single_image(cls,image_name):
+        image = cls.objects.filter(image_name__icontains=image_name)
+        return image
+    @classmethod
+    def update_image(cls,current,new):
+        to_update = Image.objects.filter(image_name=current).update(image_name=new)
+        return to_update
+
+    @classmethod
+    def search_by_category(cls,search_term):
+        images = cls.objects.filter(category__category_name__icontains=search_term)
+        return images
+        
+    class Meta:
+        ordering = ['ima_name']
+
+
+
 class Location(models.Model):
     location_name = models.CharField(max_length=30)
 
@@ -40,6 +83,7 @@ class Category(models.Model):
     
     def save_category(self):
         self.save()
+
     def delete_category(self):
         self.delete()
         
