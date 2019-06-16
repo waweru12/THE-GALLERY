cf9bb382d12d4a856744ded6c2ce0da07e61eef9
Santa Claus <santa@northpole.com>
2019-06-16 22:05:10 +0300

    created static folder

--- appgallery/models.py
+++ appgallery/models.py
@@ -4,11 +4,9 @@ import datetime as dt
 
 # Create your models here.
 class Image(models.Model):
-    image = models.ImageField(upload_to = 'appgallery/', default='DEFAULT VALUE')
+    
     ima_name = models.CharField(max_length=33)
     ima_description = models.TextField()
-    location = models.ForeignKey(location)
-    category = models.ForeignKey(category)
     article_image = models.ImageField(upload_to = 'appgallery')
     post_date = models.DateTimeField(default=2)
 
--- gallery/urls.py
+++ gallery/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^appgallery/',include('appgallery.urls'))
+    url(r'^',include('appgallery.urls'))
 ]
