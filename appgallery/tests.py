0685cb9d3e48f6c2e0f3dca02418305962fa00a3
Santa Claus <santa@northpole.com>
2019-06-16 18:27:42 +0300

    created class for location test

--- appgallery/tests.py
+++ appgallery/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Image,Category,Location
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    #Set up method
+
+    def setUp(self):
+        self.nairobi = Location(location_name = 'Nairobi')
+
+    #Testing insatnce 
+
+    def test_instance (self):
+        self.assertTrue(isinstance(self.nairobi,Location))
+
+
+    # Testing save method 
+    def test_save_method(self):
+        self.nairobi.save_location()
+
